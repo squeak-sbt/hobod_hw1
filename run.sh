4676ddf3d24f88ccb7e2de8d345be88551690b25
Dmitriy Ovchinnikov <ovchinnikov.dk@phystech.edu>
2017-03-12 16:12:47 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 hadoop fs -rm -r out result
 hadoop jar jar/UrlCount.jar ru.mipt.UrlCount /data/socnet_urls out
 hadoop jar jar/UrlCount.jar ru.mipt.UrlSort out result
-hadoop fs -cat result/part-r-00000
+hadoop fs -cat result/part-r-00000 | head -40
