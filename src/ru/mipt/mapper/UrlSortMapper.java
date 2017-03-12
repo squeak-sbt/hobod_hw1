55999b38c674f4af43ffe51cbe9307d13490452d
Dmitriy Ovchinnikov <ovchinnikov.dk@phystech.edu>
2017-03-12 16:40:48 +0300

    Mapper

--- src/ru/mipt/mapper/UrlSortMapper.java
+++ src/ru/mipt/mapper/UrlSortMapper.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import ru.mipt.writable_comparable.DomainCount;
 import ru.mipt.writable_comparable.SocnetCount;
 
 import java.io.IOException;
@@ -11,10 +12,11 @@ import java.io.IOException;
 /**
  * Created by dmitry on 12.03.17.
  */
-public class UrlSortMapper extends Mapper<LongWritable, Text, SocnetCount, Text>{
+public class UrlSortMapper extends Mapper<LongWritable, Text, SocnetCount, DomainCount>{
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] fields = value.toString().split("\t");
-        context.write(new SocnetCount(new Text(fields[0]), new IntWritable(Integer.valueOf(fields[2]))), new Text(fields[1]));
+        context.write(new SocnetCount(new Text(fields[0]), new IntWritable(Integer.valueOf(fields[2]))),
+                new DomainCount(new Text(fields[1]), new IntWritable(Integer.valueOf(fields[2]))));
     }
 }
