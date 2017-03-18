a31729a2a51ba281cffc5b48adc1748ef29be03f
Dmitriy Ovchinnikov <ovchinnikov.dk@phystech.edu>
2017-03-18 02:11:00 +0100

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-hadoop fs -rm -r out > /dev/null
-hadoop jar jar/UrlCount.jar ru.mipt.UrlCount /data/socnet_urls out > /dev/null
+hadoop fs -rm -r out >/dev/null 2>&1
+hadoop jar jar/UrlCount.jar ru.mipt.UrlCount /data/socnet_urls out >/dev/null 2>&1
 hadoop fs -cat out/part-r-00000 | head -40
